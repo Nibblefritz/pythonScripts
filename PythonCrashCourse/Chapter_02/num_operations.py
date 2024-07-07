bbc995f9a58afe317f5129f1cb3d61c74b650535
Nibblefritz <kyle@fifieldonline.com>
2024-07-07 08:42:14 -0600

    adding num_operations.py to Chapter_02

---
+++ PythonCrashCourse/Chapter_02/num_operations.py
@@ -0,0 +1,20 @@
+print("5 + 3")
+print(5 + 3)
+
+print("10 - 4")
+print(5 + 3)
+
+print("4 * 2")
+print(4 * 2)
+
+print("16 / 2")
+print(16 / 2)
+
+print("2 ** 3")
+print(2 ** 3)
+
+print("8 % 2")
+print(8 % 2)
+
+favorite_number = 13
+print(f"My favorite number is {favorite_number}")
